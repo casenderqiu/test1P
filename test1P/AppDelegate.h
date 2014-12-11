d9c51f35b938889a7872ff349a0f0642007c0ee9
Deanqiu <dean@DeanqiudeMac-mini.local>
2014-12-11 14:00:45 +0800

    Initial Commit

---
+++ test1P/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test1P
+//
+//  Created by Deanqiu on 14/12/11.
+//  Copyright (c) 2014年 Deanqiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
